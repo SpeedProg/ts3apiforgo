12730c3fd671dd8e3beb112a544523136f855aa8
SpeedProg <constantin.wenger@googlemail.com>
2014-04-21 16:31:16 +0200

    Added Serverlist to API

--- TS3ApiExportedMethods.go
+++ TS3ApiExportedMethods.go
@@ -340,6 +340,7 @@ func (api TS3Api) Instanceedit(properties [][]string) (qerr QueryError) {
 	return
 }
 
+// VSMachinId == -1 => no machin id
 type ServerListEntry struct {
 	VSId        int
 	VSPort      int
@@ -347,10 +348,78 @@ type ServerListEntry struct {
 	VSClOnline  int
 	VSQClOnline int
 	VSMaxCl     int
-	VSUTime     int64
+	VSUTime     uint64
 	VSName      string
 	VSAutostrt  bool
 	VSMachinId  int
 	VSUID       string
 }
 
+func (entry *ServerListEntry) parseMsg(s string) {
+	params := strings.Split(s, " ")
+	for _, param := range params {
+		if strings.Contains(param, "=") {
+			kvpair := strings.SplitN(param, "=", 2)
+			entry.parseParam(kvpair[0], kvpair[1])
+		} else {
+			entry.parseParam(param, "")
+		}
+	}
+}
+
+func (entry *ServerListEntry) parseParam(k, v string) (err error) {
+	switch k {
+	case "virtualserver_id":
+		entry.VSId, err = strconv.Atoi(v)
+	case "virtualserver_port":
+		entry.VSPort, err = strconv.Atoi(v)
+	case "virtualserver_status":
+		entry.VSStatus = v
+	case "virtualserver_clientsonline":
+		entry.VSClOnline, err = strconv.Atoi(v)
+	case "virtualserver_queryclientsonline":
+		entry.VSQClOnline, err = strconv.Atoi(v)
+	case "virtualserver_maxclients":
+		entry.VSMaxCl, err = strconv.Atoi(v)
+	case "virtualserver_uptime":
+		entry.VSUTime, err = strconv.ParseUint(v, 10, 64)
+	case "virtualserver_name":
+		entry.VSName = v
+	case "virtualserver_autostart":
+		entry.VSAutostrt, err = getBoolFromString(v)
+	case "virtualserver_machine_id":
+		if v == "" {
+			entry.VSMachinId = -1
+		} else {
+			entry.VSMachinId, err = strconv.Atoi(v)
+		}
+	case "virtualserver_unique_identifier":
+		entry.VSUID = v
+	default:
+		logger.Error("%s=%s is not a valid param of serverlist.", k, v)
+		err = errors.New(k + "=" + v + " is not a valid param of serverlist")
+	}
+	return
+}
+
+// Gives a list.List of struct ServerListEntry
+func (api TS3Api) Serverlist(all bool, onlyoffline bool, uid bool) (serverlist list.List, qerr QueryError) {
+	cmd := "serverlist"
+	if uid {
+		cmd += " -uid"
+	}
+	if all {
+		cmd += " -all"
+	}
+	if onlyoffline {
+		cmd += " -onlyoffline"
+	}
+	var alist *list.List
+	alist, qerr = api.doCommand(cmd)
+	for e := alist.Front(); e != nil; e = e.Next() {
+		sEntry := ServerListEntry{}
+		sEntry.parseMsg(e.Value.(string))
+		serverlist.PushBack(sEntry)
+	}
+	return
+}
